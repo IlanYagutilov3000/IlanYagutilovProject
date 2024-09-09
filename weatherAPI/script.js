f636a2e99f0e29ed231e608be9986fb8aa8a23f9
IlanYagutilov3000 <ilanyagotilov191@gmail.com>
2024-09-09 21:17:30 +0300

    update projects

---
+++ weatherAPI/script.js
@@ -0,0 +1,44 @@
+const API_KEY = "38ce246c9a6add42c091594545ea05da";
+const URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric&q=`
+const quary = document.querySelector("#inputCity");
+const button = document.querySelector("button");
+const cityName = document.querySelector("#city");
+const weatherIcon = document.querySelector("#weatherIcon");
+const temperature = document.querySelector("#temperature");
+const description = document.querySelector("#description");
+const errorMessage = document.querySelector("#errorMessage");
+
+async function getWeather(city) {
+    if (city) {
+        try {
+            const response = await fetch(URL + city);
+            const data = await response.json();
+            displayWeather(data)
+        } catch (error) {
+            console.log((Error("something went wrong", error)));
+        }
+
+    }
+}
+
+function displayWeather(weatherData) {
+    if (weatherData.cod === 200) {
+        errorMessage.textContent = "";
+        cityName.textContent = weatherData.name;
+        temperature.textContent = weatherData.main.temp + "°";
+        description.textContent = weatherData.weather[0].description;
+        weatherIcon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+
+    } else {
+        cityName.textContent = "";
+        temperature.textContent = "";
+        description.textContent = "";
+        weatherIcon.src = "";
+        errorMessage.textContent = weatherData.message;
+    }
+
+}
+
+button.addEventListener("click", () => {
+    getWeather(quary.value)
+})
